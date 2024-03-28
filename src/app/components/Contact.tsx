f83ce3feffa9b884d85daf0af0a652c7a038bf17
Auxtnn <jonnichas@gmail.com>
2024-03-28 05:50:32 -0700

    finally fixed

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -5,7 +5,7 @@ const Contact: React.FC = () => {
   return (
     <div className="bg-blue px-8 md:py-16 py-4 lg:px-16" id="contact">
       <div className="max-w-7xl mx-auto lg:flex lg:justify-between lg:items-center">
-        <div className="text-white text-2xl md:text-3xl font-bold tracking-widest lg:w-1/2 lg:text-left lg:mb-0 mb-4">
+        <div className="text-white text-2xl md:text-3xl text-center font-bold tracking-widest lg:w-1/2 lg:text-left lg:mb-0 mb-4">
           Elevate Your Projects with Us, Ensure Your Success
         </div>
         <div className="lg:w-1/2 lg:text-right text-center md:my-0 my-8 mx-auto">
